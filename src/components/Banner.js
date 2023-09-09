ef106711a839ccf42c13b9fdcacdf68fce433513
harsh lodhi <90082332+harshlodhi00@users.noreply.github.com>
2023-09-09 18:57:54 +0530

    Setup React

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Banner.css"
+
+export const Banner = () => {
+  return (
+    <>
+      <div className="banner">📣 Interviews is now in beta! Read more →</div>
+    </>
+  );
+};
